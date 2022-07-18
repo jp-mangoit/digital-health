d2085559e26f49ed2b85c1c638309c8ed2cfb14a
Pratik baberwal <pratik.baberwal@mangoitsolutions.in>
2022-07-18 17:37:14 +0530

    Creating UI structure of app changes

--- src/app/component/common/sidebar/sidebar.component.ts
+++ src/app/component/common/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
+
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    
   }
 
+  ngOnInit() {
+    
+  
+  }
+  public ngInit() {
+    $('#tree').tree({
+      dataSource: [
+        { text: 'North America', children: [ { text: 'USA', children: [ { text: 'California' }, { text: 'Miami' } ] }, { text: 'Canada' },  { text: 'Mexico' } ] },
+        { text: 'Europe', children: [ { text: 'France' },  { text: 'Spain' },  { text: 'Italy' } ] },
+        { text: 'South America', children: [ { text: 'Brazil' },  { text: 'Argentina' },  { text: 'Columbia' } ] }
+      ],
+      width: 500,
+      uiLibrary: 'bootstrap',
+      checkboxes: true
+    });
+  }
 }
