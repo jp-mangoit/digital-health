31e2d75bf71c2923069b78e29702b5b79a7f052b
JP <jp@mangoitsolutions.in>
2022-07-20 02:21:00 +0530

    customize map

--- src/app/component/test/test.component.ts
+++ src/app/component/test/test.component.ts
@@ -32,54 +32,96 @@ export class TestComponent implements OnInit {
   };
 
   ngOnInit(): void {
-
     // Create root and chart
     let root = am5.Root.new('chartdiv');
-    let chart = root.container.children.push(
-      am5map.MapChart.new(root, { projection: am5map.geoMercator() })
+
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
+
+    let chart: any = root.container.children.push(
+      am5map.MapChart.new(root, {
+        panX: 'none',
+        panY: 'none',
+        wheelX: 'none',
+        wheelY: 'none',
+        projection: am5map.geoMercator(),
+      })
     );
 
     // Create polygon series
     let polygonSeries = chart.series.push(
       am5map.MapPolygonSeries.new(root, {
         geoJSON: am5geodata_worldLow,
-        exclude: ["AQ"]
+        exclude: ['AQ'],
       })
     );
 
-    let cities:any = {
-      "type": "FeatureCollection",
-      "features": [{
-        "type": "Feature",
-        "properties": {
-          "name": "India",
-          "id": "India"
+    // let tooltip:any = am5.Tooltip.new(root, {
+    //   getFillFromSprite: false,
+    //   labelText: "[bold]{name}"
+    // });
+
+    // tooltip.get("background").setAll({
+    //   fill: am5.color(0x00ffff),
+    //   fillOpacity: 0.8
+    // });
+
+    //polygonSeries.set("tooltip", tooltip);
+
+    polygonSeries.set("fill", am5.color(0xffffff));
+    polygonSeries.set("stroke", am5.color(0x000000));
+
+    polygonSeries.mapPolygons.template.setAll({
+      tooltipText: '{name}',
+      toggleKey: "active",
+
+      templateField: 'polygonSettings',
+      interactive: true,
+    });
+
+    polygonSeries.mapPolygons.template.states.create('hover', {
+      fill: am5.color(0x677935),
+    });
+    
+    polygonSeries.mapPolygons.template.states.create("active", {
+      fill: root.interfaceColors.get("primaryButtonActive")
+    });
+
+    let cities: any = {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          properties: {
+            name: 'India',
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [78.96288, 20.593684],
+          },
         },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [78.96288, 20.593684]
-        }
-      }, {
-        "type": "Feature",
-        "properties": {
-          "name": "London",
-          "id": "London"
+        {
+          type: 'Feature',
+          properties: {
+            name: 'London',
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [-0.454296, 51.47002],
+          },
         },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [-0.454296, 51.470020]
-        }
-      }, {
-        "type": "Feature",
-        "properties": {
-          "name": "Beijing",
-          "id": "Beijing"
+        {
+          type: 'Feature',
+          properties: {
+            name: 'Beijing',
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [116.597504, 40.072498],
+          },
         },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [116.597504, 40.072498]
-        }
-      }]
+      ],
     };
 
     //Create point series
@@ -90,29 +132,19 @@ export class TestComponent implements OnInit {
       })
     );
 
-    // pointSeries.data.setAll([{
-    //   geometry: {
-    //     type: "Point",
-    //     coordinates: [78.96288, 20.593684]
-    //   }
-    // }, {
-    //   geometry: {
-    //     type: "Point",
-    //     coordinates: [-0.454296, 51.470020]
-    //   }
-    // }, {
-    //   geometry: {
-    //     type: "Point",
-    //     coordinates: [116.597504, 40.072498]
-    //   }
-    // }]);
-
     pointSeries.bullets.push(function () {
+      let circle = am5.Circle.new(root, {
+        radius: 3,
+        fill: am5.color(0xff0000),
+        tooltipText: "{name}"
+      });
+    
+      circle.events.on("click", function(ev) {
+        console.log(ev.target.dataItem);
+      });
+    
       return am5.Bullet.new(root, {
-        sprite: am5.Circle.new(root, {
-          radius: 5,
-          fill: am5.color(0xff0000),
-        }),
+        sprite: circle
       });
     });
   }
