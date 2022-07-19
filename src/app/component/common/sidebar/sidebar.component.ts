a1978ed6310691689526e9eb47fb9417f1f2858d
Pratik baberwal <pratik.baberwal@mangoitsolutions.in>
2022-07-19 17:31:34 +0530

    work on ndhs pages implementation

--- src/app/component/common/sidebar/sidebar.component.ts
+++ src/app/component/common/sidebar/sidebar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NestedTreeControl } from '@angular/cdk/tree';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 import * as $ from 'jquery';
-const TREE_DATA: any = [ 
+const TREE_DATA: any = [
 
   {
     name: 'NDHS map',
@@ -29,7 +29,7 @@ const TREE_DATA: any = [
   },
   {
     name: 'Digital R&D Map',
-    
+
   },
 ];
 
@@ -48,28 +48,28 @@ export class SidebarComponent implements OnInit {
   hasChild = (_: number, node: any) =>
     !!node.children && node.children.length > 0;
 
-  
+
   public ngOnInit() {
     $(document).ready(function () {
-         $('.toggleMenubar').on('click', function () {
-           $('body').toggleClass('site-menubar-unfold').toggleClass('site-menubar-fold');
-         });
-       });
-      $('.site-menu-item ul').on('click', function(e) {
-        $('.site-menu-item ul li ul.co-sub-menu').toggleClass("level-3"); //you can list several class names 
-        e.preventDefault();
+      $('.toggleMenubar').on('click', function () {
+        $('body').toggleClass('site-menubar-unfold').toggleClass('site-menubar-fold');
       });
-}
-dropDown3() {
-  $('.toggleSubmenu3').next('ul').toggleClass('show');
-}
-dropDown2() {
-  $('.toggleSubmenu2').next('ul').toggleClass('show');
-}
-dropDown1() {
-  $('.toggleSubmenu1').next('ul').toggleClass('show');
-}
-isValue: number = 0;
+    });
+    $('.site-menu-item ul').on('click', function (e) {
+      $('.site-menu-item ul li ul.co-sub-menu').toggleClass("level-3"); //you can list several class names 
+      e.preventDefault();
+    });
+  }
+  dropDown3() {
+    $('.toggleSubmenu3').next('ul').toggleClass('show');
+  }
+  dropDown2() {
+    $('.toggleSubmenu2').next('ul').toggleClass('show');
+  }
+  dropDown1() {
+    $('.toggleSubmenu1').next('ul').toggleClass('show');
+  }
+  isValue: number = 0;
 
-toggle(num:number) { this.isValue = num; }
+  toggle(num: number) { this.isValue = num; }
 }
